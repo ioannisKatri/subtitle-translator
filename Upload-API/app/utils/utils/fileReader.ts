5822d4a8077b343988d32fefeade115d068b0ab1
Ioannis Katrimpouzas <36813346+ioannisKatri@users.noreply.github.com>
2021-06-30 17:39:40 +0200

    Update fileReader.ts

--- Upload-API/app/utils/utils/fileReader.ts
+++ Upload-API/app/utils/utils/fileReader.ts
@@ -3,5 +3,5 @@ import fs from "fs";
 
 export const fileReader = async (path: string, file: string): Promise<string> => {
     let readFile = util.promisify(fs.readFile);
-    return await readFile(`${path}/${file}`, {encoding: "utf8"});
+    return await readFile(`${path}/${file}` , {encoding: "utf8"});
 }
